dfa1aa824b82bae72d063a2d5373151c4b1dccd5
Taufik Agung Santoso <taufikagungsantoso17@gmail.com>
2020-06-11 07:01:48 +0700

    Initial commit

---
+++ application/config/autoload.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+$autoload['libraries'] = array('session','database', 'email','form_validation', 'upload','encryption', 'pagination');
+$autoload['drivers'] = array();
+$autoload['helper'] = array('url','form','email','captcha');
+$autoload['config'] = array();
+$autoload['language'] = array();
+$autoload['model'] = array();
